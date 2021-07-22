95716f4d5fe8393e06c9351a3e7dfafeaca2296c
rusty-chris <rustydataltd@gmail.com>
2021-07-22 09:22:48 +0100

    removed dummy sinusoidal data, now plotting real data. Fixed plot persistance and stop/start behaviour

--- pages/simulation.py
+++ pages/simulation.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import altair as alt
 import time
 import pickle
@@ -17,32 +15,35 @@ def play_label(playing):
 def handle_play_click():
     st.session_state.playing = not st.session_state.playing
 
-    if st.session_state.playing:
-        st.session_state.rep = randint(0, 9)
+    if st.session_state.global_time == 99:
+        st.session_state.global_time = 0
+
+        replicate = randint(0, 9)
+        while replicate == st.session_state.replicate:
+            replicate = randint(0, 9)
+            st.session_state.replicate = replicate
+
+        st.session_state.data = load_data(replicate)
 
 
 def load_data(rep=0):
     with open('data/model_vars_rep_%d.pickle' % rep, 'rb') as ifile:
         df = pickle.load(ifile)
+        df['time'] = df.index
 
     return df
 
 
 def page_code():
 
-    replicate = (
-        st.session_state.rep
-        if 'rep' in st.session_state
-        else 0
-    )
-    data = load_data(replicate)
-    data['time'] = data.index
-    print(data.head())
-    print('time' in data.columns)
+    if 'replicate' not in st.session_state:
+        st.session_state.replicate = 0
+
+    if 'data' not in st.session_state:
+        st.session_state.data = load_data(st.session_state.replicate)
 
     st.title("Simulation")
     st.write("This tab will show animation of a simulation (approximately like running the Mesa server).")
-    st.write("(Note: Currently just showing dummy sinusoidal data.")
 
     speed = st.sidebar.slider(
             "Set simulation speed:",
@@ -54,13 +55,16 @@ def page_code():
     if 'playing' not in st.session_state:
         st.session_state.playing = False
 
+    if 'global_time' not in st.session_state:
+        st.session_state.global_time = 0
+
     play = st.sidebar.button(
         play_label(st.session_state.playing),
         on_click=handle_play_click
     )
 
     domain_colours = dict(zip(
-        ['successful', 'failed', 'null'],
+        ['SuccessfulProjects', 'FailedProjects', 'NullProjects'],
         ['green', 'red', 'orange']
     ))
     selection = st.multiselect(
@@ -68,11 +72,6 @@ def page_code():
         options=list(domain_colours),
         default=list(domain_colours)
     )
-    column_key = {
-        'SuccessfulProjects': 'successful',
-        'FailedProjects': 'failed',
-        'NullProjects': 'null'
-    }
 
     domain = [s for s in selection]
     range_ = [domain_colours[d] for d in domain]
@@ -80,18 +79,12 @@ def page_code():
     # note: updating 'selection' was messing with the contents of the slider variables
     # hence creation of a new list 'plot_series'
 
-    df = pd.DataFrame(
-        {
-            'time': 0,
-            'successful': [np.sin(0)],
-            'failed': [np.cos(0)],
-            'null': [np.cos(0)**2],
-        }
-    )
-
     chart = st.altair_chart(
         alt.Chart(
-            df[plot_series].melt('time')).mark_line().encode(
+            st.session_state.data.loc[
+                0:st.session_state.global_time,
+                plot_series
+            ].melt('time')).mark_line().encode(
                 x=alt.X('time', axis=alt.Axis(title='timestep')),
                 y=alt.Y('value', axis=alt.Axis(title='number of projects')),
                 color=alt.Color('variable', scale=alt.Scale(domain=domain, range=range_))
@@ -99,29 +92,16 @@ def page_code():
         use_container_width=True
     )
 
-    chart2 = st.altair_chart(
-        alt.Chart(
-            data[['time', 'SuccessfulProjects', 'FailedProjects', 'NullProjects']].melt('time')).mark_line().encode(
-            x=alt.X('time', axis=alt.Axis(title='timestep')),
-            y=alt.Y('value', axis=alt.Axis(title='number of projects')),
-            color=alt.Color('variable', scale=alt.Scale(domain=list(column_key), range=range_))
-        ),
-        use_container_width=True
-    )
-
     if st.session_state.playing:
-        for t in range(1, 100):
+        start = st.session_state.global_time + 1
+
+        for t in range(start, 100):
             chart.add_rows(
-                pd.DataFrame(
-                    {
-                        'time': t,
-                        'successful': [np.sin(t / 10)],
-                        'failed': [np.cos(t / 10)],
-                        'null': [np.cos(t / 7)**2],
-                    }
-                )[plot_series].melt('time')
+                st.session_state.data.loc[t:t, plot_series].melt('time')
             )
             time.sleep(0.2 / speed)
+            st.session_state.global_time += 1
 
-    st.session_state.playing = False
-    #st.experimental_rerun()
+            if t == 99:
+                st.session_state.playing = False
+                st.experimental_rerun()
