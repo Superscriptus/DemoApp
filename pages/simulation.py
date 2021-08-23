9a64de8ac464322ce1e0ead8319d7a3081acc2b4
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-08-23 11:27:56 +0100

    Added widgets for all simulation paramters and checked correct data loading

--- pages/simulation.py
+++ pages/simulation.py
@@ -14,16 +14,22 @@ from random import choice
 
 def play_label(playing):
     if playing:
-        return 'Stop'
+        return 'Stop simulation'
     else:
-        return 'Play'
+        return 'Play simulation'
 
 
 def reload():
 
     st.session_state.global_time = 0
     replicate = choice([i for i in range(10) if i != st.session_state.replicate])
-    load_data(st.session_state.project_count, st.session_state.dept_workload, replicate)
+    load_data(
+        st.session_state.project_count,
+        st.session_state.dept_workload,
+        st.session_state.budget_func,
+        st.session_state.skill_decay,
+        st.session_state.train_load,
+        replicate)
 
 
 def handle_play_click():
@@ -50,14 +56,17 @@ def unpickle(file_path):
         return None
 
 
-def load_data(project_count, dept_workload, rep):
+def load_data(project_count, dept_workload, budget_func, skill_decay, train_load, rep):
 
     optimiser_dict = dict(zip(
         ['Random', 'Optimised', 'Flexible start time'],
         ['Random', 'Basin', 'Basin_w_flex']
     ))
 
-    sub_dir = "pps_%d_dwl_%.1f_budget_%d_sd_%.3f_train_%.1f" % (project_count, dept_workload, 1, 0.99, 0.1)
+    sub_dir = (
+            "pps_%d_dwl_%.1f_budget_%d_sd_%.3f_train_%.1f"
+            % (project_count, dept_workload, budget_func, skill_decay, train_load)
+    )
     st.session_state.data = unpickle(
         "data/" + sub_dir + "/%s/model_vars_rep_%d.pickle" % (optimiser_dict[st.session_state.team_allocation], rep)
     )
@@ -154,40 +163,62 @@ def create_sidebar_controls():
 
     if 'project_count' not in st.session_state:
         st.session_state.project_count = 2
+    if 'budget_func' not in st.session_state:
+        st.session_state.budget_func = True
 
-    team_allocation = st.sidebar.radio(
-        "Team allocation method:",
-        options=['Random', 'Optimised', 'Flexible start time'],
-        key='team_allocation',
-        on_change=reload,
-        help="The method used for allocating a team of workers to each project.  \n"
-             "* Random: randomly assigned team.  \n"
-             "* Optimised: success probability optimised using basin-hopping algorithm.  \n"
-             "* Flexible start time: same as _Optimised_ but with flexible project start time."
-    )
+    row_0 = st.sidebar.beta_columns([2, 1])
 
-    # Note: 'key' links the widget to session state variable of same name. This is not documented behaviour?!
-    project_count = st.sidebar.selectbox(
-        "Number of new projects created each time step:",
-        options=[1, 2, 3, 5, 10],
-        key='project_count',
-        on_change=reload
-    )
+    with row_0[0]:
+        team_allocation = st.selectbox(
+            "Team allocation:",
+            options=['Random', 'Optimised', 'Flexible start time'],
+            key='team_allocation',
+            on_change=reload,
+            help="The method used for allocating a team of workers to each project.  \n"
+                 "* Random: randomly assigned team.  \n"
+                 "* Optimised: success probability optimised using basin-hopping algorithm.  \n"
+                 "* Flexible start time: same as _Optimised_ but with flexible project start time."
+        )
+
+    with row_0[-1]:
+        budget_func = st.selectbox(
+            "Budget:",
+            options=[True, False],
+            key='budget_func',
+            on_change=reload,
+            format_func=lambda x: 'On' if x else 'Off',
+            help="Budgetary constraint on/off."
+        )
+
+    row_1 = st.sidebar.beta_columns([1, 1])
+
+    with row_1[0]:
+        # Note: 'key' links the widget to session state variable of same name. This is not documented behaviour?!
+        project_count = st.selectbox(
+            label="New projects:",
+            options=[1, 2, 3, 5, 10],
+            key='project_count',
+            on_change=reload,
+            help="Number of new projects created each time step:"
+        )
 
     if 'dept_workload' not in st.session_state:
         st.session_state.dept_workload = 0.1
 
-    dept_workload = st.sidebar.radio(
-        "Departmental workload:",
-        options=[0.1, 0.3],
-        key='dept_workload',
-        on_change=reload
-    )
+    with row_1[-1]:
+        dept_workload = st.slider(
+            "Departmental workload:",
+            min_value=0.1,
+            max_value=0.3,
+            step=0.2,
+            key='dept_workload',
+            on_change=reload
+        )
 
     if 'skill_decay' not in st.session_state:
         st.session_state.skill_decay = 0.99
 
-    skill_decay = st.sidebar.radio(
+    skill_decay = st.sidebar.selectbox(
         "Skill decay:",
         options=[0.950, 0.990, 0.995],
         key='skill_decay',
@@ -195,10 +226,24 @@ def create_sidebar_controls():
         format_func=lambda x: '%.3f' % x
     )
 
+    if 'train_load' not in st.session_state:
+        st.session_state.train_load = 0.1
+
+    train_load = st.sidebar.selectbox(
+        "Training load:",
+        options=[0.0, 0.1, 0.3, 2.0],
+        key='train_load',
+        on_change=reload,
+        format_func=lambda x: '%.1f' % x if x < 2 else 'Boost'
+    )
+
     if 'data' not in st.session_state:
         load_data(
             project_count=st.session_state.project_count,
             dept_workload=st.session_state.dept_workload,
+            budget_func=st.session_state.budget_func,
+            train_load=st.session_state.train_load,
+            skill_decay=st.session_state.skill_decay,
             rep=st.session_state.replicate
         )
 
