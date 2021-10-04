80394f858166d2b90398a518fbd92624fe00f006
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-10-04 17:24:21 +0100

    Added test bar chart in Comparison page

--- pages/comparison.py
+++ pages/comparison.py
@@ -1,39 +1,69 @@
+"""
+TODO:
+- refactor timeseries plot (use class from simulation?)
+-
+"""
+import pandas as pd
 import streamlit as st
-from .utilities import load_models
+import numpy as np
+import altair as alt
 
 
 def page_code():
 
     comparison_data = {}
 
-    for preset in ['A', 'B', 'C', 'D']:
-        parameter_dict = st.session_state.config.simulation_presets[preset]
-
-        comparison_data[preset] = load_models(
-            project_count=parameter_dict['project_count'],
-            dept_workload=parameter_dict['dept_workload'],
-            budget_func=parameter_dict['budget_func'],
-            train_load=parameter_dict['train_load'],
-            skill_decay=parameter_dict['skill_decay'],
-            rep=st.session_state.replicate,
-            team_allocation=parameter_dict['team_allocation']
-        )
+    domain = list(st.session_state.config.simulation_presets.keys())
+    colours = ['blue', 'orange', 'green', 'red']
 
     st.title("Comparison")
 
-    st.write("This tab will allow comparison between two selected simulations.")
-    option = st.multiselect(
-        'Select team allocators:',
-        ('Random',
-         'Basic',
-         'Niter0',
-         'Basin',
-         'Basin_w_flex'
-         )
-    )
-    color = st.select_slider(
-        'Select skill decay value:',
-        options=[0.95, 0.99, 0.995]
-    )
-    st.image('images/projects_timeline_flex.png')
+    st.write("Here we compare the presets...(more details to follow).")
+
+    chart_data = None
+    for preset in st.session_state.config.simulation_presets:
+
+        if chart_data is None:
+            chart_data = (
+                st.session_state.comparison_data[preset]['model_vars'][['time', 'Roi']]
+                .copy().rename(columns={'Roi': 'value'})
+            )
+            chart_data['variable'] = [preset for i in range(len(chart_data))]
+
+        else:
+            temp_data = (
+                st.session_state.comparison_data[preset]['model_vars'][['time', 'Roi']]
+                .copy().rename(columns={'Roi': 'value'})
+            )
+            temp_data['variable'] = [preset for i in range(len(temp_data))]
+            chart_data = chart_data.append(temp_data)
+
+    _x = alt.X('time', axis=alt.Axis(title='timestep'))
+
+    chart = alt.Chart(chart_data).mark_line().encode(
+        x=_x,
+        y=alt.Y('value', axis=alt.Axis(title='ROI')),
+        color=alt.Color(
+            'variable', scale=alt.Scale(
+                domain=domain,
+                range=colours
+            )
+        ),
+    ).properties(title="ROI Comparison")
+
+    st.altair_chart(chart, use_container_width=True)
+
+    st.subheader("Bar chart test:")
+
+    bar_data = pd.DataFrame({
+        preset: [
+            np.mean(st.session_state.comparison_data[preset]['model_vars'].loc[-25:]['Roi'])
+            if d == preset
+            else 0
+            for d in domain
+        ]
+        for preset in domain
+    }, index=domain)
+
+    st.bar_chart(bar_data)
 
