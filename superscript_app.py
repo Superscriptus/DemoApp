bb596eec21a01be1d5af2a84640619085bddca6b
rusty-chris <rustydataltd@gmail.com>
2021-12-28 11:38:46 +0000

    reverted to secrets.toml for firestore credentials

--- superscript_app.py
+++ superscript_app.py
@@ -17,11 +17,15 @@ Resources:
 from application import Application
 from pages import simulation, comparison, about
 from streamlit import secrets
+import json
 
 app = Application()
 app.create_page("About", about)
 app.create_page("Simulation", simulation)
 # app.create_page("Hypotheses", hypotheses)
 app.create_page("Comparison", comparison)
+
+with open(secrets["FIRESTORE_KEY_FILE"], "w") as ofile:
+    json.dump(secrets["FIRESTORE"], ofile, indent=4)
 
 app.execute()
