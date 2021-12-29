7ee1a232e4ffc1841aef93a4bccf2eacf1ba7be2
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-12-29 09:49:25 +0000

    updated data transfer script

--- data_transfer_complete.py
+++ data_transfer_complete.py
@@ -41,16 +41,17 @@ def strip_unwanted(path, max_reps=True):
             os.remove(f)
 
     for f in glob.glob(to_path + "/*/network_rep_*_timestep_*"):
-        _t = f.split('_')[4].split('.')[0]
-        if _t > 1:
+        file_name = f.split('/')[-1]
+        _t = file_name.split('_')[4].split('.')[0]
+        if int(_t) > 1:
             os.remove(f)
 
     if max_reps:
         for f in glob.glob(to_path + "/*/*"):
             file_name = f.split('/')[-1]
             if 'rep' in file_name:
-                rep_num = file_name.split('rep')[1].split('_')[1]
-                if rep_num > MAX_REP:
+                rep_num = file_name.split('rep')[1].split('_')[1].split('.')[0]
+                if int(rep_num) > MAX_REP:
                     os.remove(f)
                 # if 'network' in file_name:
                 #     if int(file_name.split('_')[2]) > MAX_REP:
